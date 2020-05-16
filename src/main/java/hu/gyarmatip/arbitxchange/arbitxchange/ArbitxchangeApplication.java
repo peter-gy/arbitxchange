07f026de06d613deee9f4f6ab97c694fb12ca0b7
Peter Gyarmati <gyarmatipeti01@gmail.com>
2020-05-16 09:16:52 +0200

    initial commit

---
+++ src/main/java/hu/gyarmatip/arbitxchange/arbitxchange/ArbitxchangeApplication.java
@@ -0,0 +1,13 @@
+package hu.gyarmatip.arbitxchange.arbitxchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArbitxchangeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArbitxchangeApplication.class, args);
+    }
+
+}
